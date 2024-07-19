1b81112ef6c4182fc6903e209a99ee6f034421e6
Daniel Edwards <Daniel.Edwards@vector.com>
2024-07-19 08:51:59 +0200

    ci-staging: put container setup into batch-file

---
+++ containers/ci-windows/setup-container.bat
@@ -0,0 +1,16 @@
+@REM note: curl is pre-installed in nanoserver container-images
+
+curl -SL --output vs_buildtools.exe https://aka.ms/vs/17/release/vs_buildtools.exe
+
+START /w vs_buildtools.exe `
+    --quiet --wait --norestart --nocache `
+    --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools" `
+    --add Microsoft.VisualStudio.Component.VC.v141.x86.x64
+
+IF "%ERRORLEVEL%"=="3010" EXIT 0
+
+DEL /q vs_buildtools.exe
+
+@REM --remove Microsoft.VisualStudio.Component.Windows10SDK.10240
+@REM --remove Microsoft.VisualStudio.Component.Windows10SDK.10586
+@REM --remove Microsoft.VisualStudio.Component.Windows10SDK.14393
