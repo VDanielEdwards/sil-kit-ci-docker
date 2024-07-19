0106d2de3a7c8bf0a09d5608fbe440771a15910c
Daniel Edwards <Daniel.Edwards@vector.com>
2024-07-19 11:28:36 +0200

    ci-staging: disable installing build-tools

--- containers/ci-windows/setup-container.bat
+++ containers/ci-windows/setup-container.bat
@@ -10,12 +10,12 @@ IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
 @REM install build-tools components
 
-START /w vs_buildtools.exe ^
-    --quiet --wait --norestart --nocache ^
-    --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools" ^
-    --add Microsoft.VisualStudio.Component.VC.v141.x86.x64
+@REM START /w vs_buildtools.exe ^
+@REM     --quiet --wait --norestart --nocache ^
+@REM     --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools" ^
+@REM     --add Microsoft.VisualStudio.Component.VC.v141.x86.x64
 
-IF %ERRORLEVEL% NEQ 0 IF %ERRORLEVEL% NEQ 3010 EXIT %ERRORLEVEL%
+@REM IF %ERRORLEVEL% NEQ 0 IF %ERRORLEVEL% NEQ 3010 EXIT %ERRORLEVEL%
 
 
 @REM cleanup if everything went well
