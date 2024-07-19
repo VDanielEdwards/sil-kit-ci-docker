b0b425b0db312f53eb56fbd629c2d9eead819ac5
Daniel Edwards <Daniel.Edwards@vector.com>
2024-07-19 09:40:29 +0200

    ci-staging: switch back to servercore
    vs_buildtools.exe required .net

--- containers/ci-windows/setup-container.bat
+++ containers/ci-windows/setup-container.bat
@@ -1,14 +1,14 @@
 @REM note: curl is pre-installed in nanoserver container-images
 
 curl -SL --output C:\TEMP\vs_buildtools.exe https://aka.ms/vs/17/release/vs_buildtools.exe
-IF NOT "%ERRORLEVEL%"=="0" EXIT
+IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
 START /w C:\TEMP\vs_buildtools.exe ^
     --quiet --wait --norestart --nocache ^
     --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools" ^
     --add Microsoft.VisualStudio.Component.VC.v141.x86.x64
 
-IF NOT "%ERRORLEVEL%"=="0" IF NOT "%ERRORLEVEL%"=="3010" EXIT /b
+IF %ERRORLEVEL% NEQ 0 IF %ERRORLEVEL% NEQ 3010 EXIT %ERRORLEVEL%
 
 DEL /q vs_buildtools.exe
 
