db1d2bcc6c8cc88869eb1018133e8b70c5fc8a95
Daniel Edwards <Daniel.Edwards@vector.com>
2024-07-19 09:51:56 +0200

    ci-staging: add some more comments to the setup script

--- containers/ci-windows/setup-container.bat
+++ containers/ci-windows/setup-container.bat
@@ -1,17 +1,23 @@
-@REM note: curl is pre-installed in nanoserver container-images
-
-curl -SL --output C:\TEMP\vs_buildtools.exe https://aka.ms/vs/17/release/vs_buildtools.exe
+CD C:\TEMP
 IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
 
-START /w C:\TEMP\vs_buildtools.exe ^
+@REM download the build-tools setup
+@REM note: curl is pre-installed servercore container-images
+
+curl -SL --output vs_buildtools.exe https://aka.ms/vs/17/release/vs_buildtools.exe
+IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
+
+
+@REM install build-tools components
+
+START /w vs_buildtools.exe ^
     --quiet --wait --norestart --nocache ^
     --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2022\BuildTools" ^
     --add Microsoft.VisualStudio.Component.VC.v141.x86.x64
 
 IF %ERRORLEVEL% NEQ 0 IF %ERRORLEVEL% NEQ 3010 EXIT %ERRORLEVEL%
 
-DEL /q vs_buildtools.exe
 
-@REM --remove Microsoft.VisualStudio.Component.Windows10SDK.10240
-@REM --remove Microsoft.VisualStudio.Component.Windows10SDK.10586
-@REM --remove Microsoft.VisualStudio.Component.Windows10SDK.14393
+@REM cleanup if everything went well
+
+DEL /q vs_buildtools.exe
